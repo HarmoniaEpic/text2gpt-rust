835eea01b24ac9b5e5773374d0b099fec8e0c297
HarmoniaEpic <eiriishida@gmail.com>
2025-06-26 11:18:44 +0900

    Update mod.rs

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,16 @@
 pub mod folder_naming;
+pub mod ollama;
 
 pub use folder_naming::FolderNameBuilder;
+pub use ollama::{
+    check_ollama_running,
+    get_installed_models,
+    categorize_model,
+    format_model_list_for_display,
+    show_installation_hints,
+    generate_category_warning,
+    show_ollama_not_running_error,
+    show_no_models_error,
+    InstalledModel,
+    ModelCategory,
+};
